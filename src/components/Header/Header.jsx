63ec56c6c512dd657cb745eed1f12a335a51df3b
pavlo <nadazasada@gmail.com>
2026-09-19 13:27:14 +0300

    tests passed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,24 +4,24 @@
 
 // Add a default export statement for Header component to use it in the other files
 
-import './Header.css'
+import './Header.css';
 
-function Header () {
+function Header() {
   return (
     <header className="header">
-        <h1 className="header__title">Site Name</h1>
-        <nav className="navigation">
-          <a className="navigation__link" href="#about">
-            About
-          </a>
-          <a className="navigation__link" href="#services">
-            Services
-          </a>
-          <a className="navigation__link" href="#contact">
-            Contact
-          </a>
-        </nav>
-      </header>
+      <h1 className="header__title">Site Name</h1>
+      <nav className="navigation">
+        <a className="navigation__link" href="#about">
+          About
+        </a>
+        <a className="navigation__link" href="#services">
+          Services
+        </a>
+        <a className="navigation__link" href="#contact">
+          Contact
+        </a>
+      </nav>
+    </header>
   );
 }
 
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -9,10 +9,9 @@ import './Welcome.css';
 function Welcome() {
   return (
     <section className="welcome">
-        <span className="welcome__text">Sticky Header!</span>
-      </section>
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
   );
 }
 
 export default Welcome;
-
